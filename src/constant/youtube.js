ad923edd1b7b45d2eb62ea7b381718f7e395a8bb
karishmashaw00 <141134785+karishmashaw00@users.noreply.github.com>
2025-06-12 17:25:12 +0530

    Update youtube.js

--- src/constant/youtube.js
+++ src/constant/youtube.js
@@ -1,4 +1,4 @@
-export const API_KEY = "AIzaSyBofrqbFRxZ5Cs4cWH5_nZkqAuEn6x5tAY";
+export const API_KEY = "";
 export const BASE_URL = "https://www.googleapis.com/youtube/v3";
 export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&regionCode=IN&key=${API_KEY}`;
 export const SEARCH_SUGGESTION_API ="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
